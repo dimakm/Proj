16d3f8e99b4ee0a0def8e397022b33fea893842e
Dima Kroma <37256188+dimakm@users.noreply.github.com>
2018-09-28 12:39:38 -0400

    Update TheMap.js

--- src/TheMap.js
+++ src/TheMap.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
-import TheMarker from './TheMarker';
 
 const TheMap = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={props.zoom}
-    defaultCenter={props.center}
-  ><TheMarker center={props.center}/>}
+    defaultCenter={props.center}>
   </GoogleMap>
 ))
 
 export default TheMap;
+
